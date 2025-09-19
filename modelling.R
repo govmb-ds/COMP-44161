826eb78aa5800fa3b731854cc017268e5a113769
gom-ta <153634606+gom-ta@users.noreply.github.com>
2025-09-19 11:20:59 -0500

    Add in summary call

--- modelling.R
+++ modelling.R
@@ -5,3 +5,5 @@ file = "~/44161/Assessment_Parcels_20250919.csv"
 
 df <- loadData(file)
 model <- lm('Total.Assessed.Value.Num ~ Total.Living.Area.Num + Assessed.Land.Area.Num', df)
+
+summary(model)
