8349311d15f2c25b2ea182676a677858e914fb95
gom-ta <153634606+gom-ta@users.noreply.github.com>
2025-09-19 11:19:53 -0500

    Create modelling.R

---
+++ modelling.R
@@ -0,0 +1,7 @@
+## Run model
+source(data.R)
+
+file = "~/44161/Assessment_Parcels_20250919.csv"
+
+df <- loadData(file)
+model <- lm('Total.Assessed.Value.Num ~ Total.Living.Area.Num + Assessed.Land.Area.Num', df)
